007c499ff41c0847e014f4b7f2b6c426b96df7db
Nguyen Tuan Anh Vu <bopvip2002@gmail.com>
2025-05-25 17:30:42 +0700

    Initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+const categories = [
+  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+];
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
